2a380e7970563d17f502eb0b530db83add8111ca
John Lynch <john.lynch@avantcredit.com>
2014-05-03 22:04:18 -0500

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloclist::Application.config.secret_token = '0f562bf22cb7e82c70b5d98b6f8f979c62a774529230dfd56c4e8d4d80dd28b90ee4a99a93fc8aad4b77326a4d0b13e7ef9688a486e651f64f5152d5f84454a1'
